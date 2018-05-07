17aae5084927b5fbe4c1f9c9847c7e8a6339b8da
baqir00 <baqirkhan00@gmail.com>
2018-05-07 10:14:16 +0530

    Add files via upload

--- extension/script.js
+++ extension/script.js
@@ -2,26 +2,67 @@ var link = location.href;
 
 url(link);
 
+var polarising_score;
+
 function url(link) {
     console.log("AJAX Start");
     var data = {js_link: link}
     $.ajax({
-        url: 'https://35.185.133.86:5000/postmethod',
-        data: JSON.stringify(data),
-        type: 'POST',
-        success: function (response) {
-            console.log("AJAX Success. SCORE =>");
-            console.log(response.score); //T  his works
-            chrome.runtime.sendMessage({score: response.score}, function(response) {
-              //  console.log(response.farewell);
-              });
+        dataType: "text",
+        type: "POST",
+        url : "https://localhost/moderator/check.php",
+        data: {
+          'url' : link,
         },
-        error: function (error) {
-            console.log("AJAX Error. Error =>");
-            console.log(error);
-        },
-        dataType: "json",
-        contentType: 'application/json;charset=UTF-8',
-    });
-
+        success : function(response){
+          if(response=="0") {
+            $.ajax({
+              url: 'https://35.185.133.86:5000/postmethod',
+              data: JSON.stringify(data),
+              type: 'POST',
+              success: function (response) {
+                    console.log("AJAX Success. SCORE =>");
+      			        console.log(response);
+                    console.log(response.score); //T  his works
+              			polarising_score = response.score;
+              			chrome.runtime.sendMessage({score: response.score}, function(response) {});
+                    $.ajax({
+                        url: 'https://localhost/moderator/insert.php',
+                        data: {
+                          'url' : link,
+                          'score' : response.score,
+                          'magnitude' : response.magnitude,
+                        },
+                        type: 'POST',
+                        success: function (response) {
+                          console.log(response);
+                        },
+                        error: function (error) {
+                            console.log(error);
+                        },
+                        dataType: "text",
+                    });
+              },
+              error: function (error) {
+                  console.log("AJAX Error. Error =>");
+                  console.log(error);
+              },
+              dataType: "json",
+              contentType: 'application/json;charset=UTF-8',
+          });
+        }
+        else {
+            console.log("else");
+            console.log(response);
+            var result = JSON.parse(response);
+            console.log(result.score);
+            polarising_score = result.score;
+            chrome.runtime.sendMessage({score: result.score}, function(result) {});
+        }
+      }
+      })
 }
+
+chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+			sendResponse(polarising_score);
+			});
--- extension/test.js
+++ extension/test.js
@@ -5,3 +5,11 @@ chrome.runtime.onMessage.addListener(
     text.innerHTML = request.score;
     console.log(request);
   });
+
+  chrome.tabs.query({'active': true,'currentWindow':true},function(tab){
+  chrome.tabs.sendMessage(tab[0].id,"stuff", function(response){
+    var text = document.getElementById('score');
+    text.innerHTML = response;
+    console.log(request);
+  });
+});
